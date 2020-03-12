ff79cf7b123a8457db04db5a6568b14543e986cc
Shoib-vvdn <57894012+Shoib-vvdn@users.noreply.github.com>
2020-03-12 16:56:54 +0530

    Update command.groovy

--- command.groovy
+++ command.groovy
@@ -1,4 +1,4 @@
 node {
   stage 'Run CE JMeter Test'
-  sh '/home/vvdn/Desktop/Jmeter/apache-jmeter-5.2.1/bin/jmeter.sh -n -t COllection_Engine_API'/s.jmx -l test.jtl'
+  sh "/home/vvdn/Desktop/Jmeter/apache-jmeter-5.2.1/bin/jmeter.sh -n -t COllection_Engine_API's.jmx -l test.jtl"
 }
